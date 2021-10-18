5ed4b82a2be45400040ce3faeeb24ee62617034d
Karl Hans Lorenz Ernits <kernit@taltech.ee>
2021-10-18 16:04:58 +0300

    Smol fix

---
+++ vet-clinic/src/Animal/AnimalList/AnimalTestData.js
@@ -0,0 +1,92 @@
+export let AnimalData = [
+    {
+        "id": 0,
+        "name": "Genevieve",
+        "dateOfBirth": "08/10/1998",
+        "species": "Strong",
+        "breed": "Hicks",
+        "chipNr": "616d693353c2c9b74503f50f",
+        "gender": "female",
+        "weight": 11946
+    },
+    {
+        "id": 1,
+        "name": "Annabelle",
+        "dateOfBirth": "08/10/1998",
+        "species": "Greer",
+        "breed": "Petty",
+        "chipNr": "616d693388bf9c614e26c33c",
+        "gender": "female",
+        "weight": 12362
+    },
+    {
+        "id": 2,
+        "name": "Chaney",
+        "dateOfBirth": "08/10/1998",
+        "species": "Preston",
+        "breed": "Hammond",
+        "chipNr": "616d6933b629cf8af7fb04f1",
+        "gender": "male",
+        "weight": 2708
+    },
+    {
+        "id": 3,
+        "name": "Rosemarie",
+        "dateOfBirth": "08/10/1998",
+        "species": "Johnston",
+        "breed": "Brewer",
+        "chipNr": "616d6933addaf37427b31a12",
+        "gender": "female",
+        "weight": 1893
+    },
+    {
+        "id": 4,
+        "name": "Leanne",
+        "dateOfBirth": "08/10/1998",
+        "species": "Anthony",
+        "breed": "Cantrell",
+        "chipNr": "616d6933f044334f2a7cf8c7",
+        "gender": "female",
+        "weight": 10860
+    },
+    {
+        "id": 5,
+        "name": "Serrano",
+        "dateOfBirth": "08/10/1998",
+        "species": "Boone",
+        "breed": "Calderon",
+        "chipNr": "616d69339f34ea90054bc676",
+        "gender": "male",
+        "weight": 17968
+    },
+    {
+        "id": 6,
+        "name": "Savage",
+        "dateOfBirth": "08/10/1998",
+        "species": "Moody",
+        "breed": "Dotson",
+        "chipNr": "616d69331d34a27ac7f7a9bd",
+        "gender": "male",
+        "weight": 16861
+    },
+    {
+        "id": 7,
+        "name": "Lacey",
+        "dateOfBirth": "08/10/1998",
+        "species": "Porter",
+        "breed": "Sandoval",
+        "chipNr": "616d6933144774d02ff036a7",
+        "gender": "female",
+        "weight": 4228
+    },
+    {
+        "id": 8,
+        "name": "Alma",
+        "dateOfBirth": "08/10/1998",
+        "species": "Brady",
+        "breed": "Padilla",
+        "chipNr": "616d693394c559fce10953f6",
+        "gender": "female",
+        "weight": 688
+    }
+]
