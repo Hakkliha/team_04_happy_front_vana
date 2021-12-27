eb474117e3383711fdb07991f0569b17b8377079
Karl Hans Lorenz Ernits <kernit@taltech.ee>
2021-12-27 17:50:03 +0200

    test

--- src/App/App.test.jsx
+++ src/App/App.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 it('renders NAV', () => {
-    render(<App />);
-    expect(screen.getByText('Owners'));
+    render(<App/>);
+    expect(screen.getByText('About'));
 });
