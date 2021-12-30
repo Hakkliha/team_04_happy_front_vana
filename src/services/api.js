8e79344b1cfe0e22e5e09fd9cb660c5a648a54c5
Karl Hans Lorenz Ernits <kernit@taltech.ee>
2021-12-31 00:45:21 +0200

    Torukontroll

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import TokenService from "./token.service";
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.0.1:8080/api",
+    baseURL: "http://13.48.57.71/:8080/api",
     headers: {
         "Content-Type": "application/json",
     },
